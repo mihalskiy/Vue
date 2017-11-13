2ce7fdbb7ac7d9a2e685377ea4bdc462e84a5c4f
mihalskiy <git config --global user.email>
2017-11-13 05:44:29 +0200

    Nesting Components

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
-// import Product from './components/Product.vue';
+import Product from './components/Product.vue';
 
-// export default [
-//    // { path: '/', component: Home },
-//     { path: '*', redirect: '/' },
-// ];
+export default [
+   // { path: '/', component: Home },
+    { path: '*', redirect: '/' },
+];
